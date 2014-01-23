30455d168c024f7b4c4705bb947d290879aeaee3
Matt Wiebe <matt@somadesign.ca>
2014-01-23 11:48:00 -0600

    CSS generator

--- css-generator.php
+++ css-generator.php
@@ -2,11 +2,13 @@
 
 class Jetpack_Custom_Fonts_Css_Generator {
 
+	const CSS_FONT_SIZE_VALUE_RX = '/((\d*\.(\d+))|(\d+))([A-Za-z]{2,3}|%)/';
+
 	/**
 	 * Holds allowed font category types. @see `jetpack_custom_fonts_rule_types` action
 	 * @var array
 	 */
-	private $allowed_types = array();
+	private $rule_types = array();
 
 	/**
 	 * Holds font rules added for a theme.
@@ -15,7 +17,7 @@ class Jetpack_Custom_Fonts_Css_Generator {
 	private $rules = array();
 
 	/**
-	 * Holds the separaotr to use when printing font CSS.
+	 * Holds the separator to use when printing font CSS.
 	 * @var string
 	 */
 	private $sep = '';
@@ -24,12 +26,26 @@ class Jetpack_Custom_Fonts_Css_Generator {
 	 * Constructor
 	 */
 	public function __construct(){
-		$default_types = array( 'body-text', 'headings' );
-		$this->allowed_types = apply_filters( 'jetpack_custom_fonts_rule_types', $default_types );
-		foreach( $this->allowed_types as $type ) {
-			$this->rules[ $type ] = array();
+		$default_types = array(
+			array(
+				'id'        => 'body-text',
+				'name'      => __( 'Body Text' ),
+				'fvdAdjust' => false,
+				'sizeRange' => 3
+			),
+			array(
+				'id'        => 'headings',
+				'name'      => __( 'Headings' ),
+				'fvdAdjust' => true,
+				'sizeRange' => 10
+			)
+		);
+		$this->rule_types = apply_filters( 'jetpack_custom_fonts_rule_types', $default_types );
+
+		foreach( $this->rule_types as $type ) {
+			$this->rules[ $type['id'] ] = array();
 		}
-		if ( defined( 'SCRIPT_DEGUG' ) && SCRIPT_DEGUG ) {
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
 			$this->sep = "\n";
 		}
 	}
@@ -70,12 +86,20 @@ class Jetpack_Custom_Fonts_Css_Generator {
 				throw new Exception( 'You must supply a \'' . $key . '\' attribute on your rule.' );
 			}
 		}
-		if ( ! in_array( $rule['type'], $this->allowed_types ) ) {
-			throw new Exception( 'Your type of '. $rule['type'] . 'is not allowed. Use one of ' . implode( ',', $this->allowed_types ) . '.' );
+		if ( ! in_array( $rule['type'], $this->get_allowed_types() ) ) {
+			throw new Exception( 'Your type of '. $rule['type'] . 'is not allowed. Use one of ' . implode( ',', $this->get_allowed_types() ) . '.' );
 		}
 		if ( ! is_array( $rule['rules'] ) || empty( $rule['rules'] ) ) {
 			throw new Exception( 'You must supply at least one array in the $rule[\'rules\'] array.' );
 		}
+	}
+
+	public function get_rule_types() {
+		return $this->rule_types;
+	}
+
+	public function get_allowed_types() {
+		return wp_list_pluck( $this->rule_types, 'id' );
 	}
 
 	/**
@@ -84,30 +108,51 @@ class Jetpack_Custom_Fonts_Css_Generator {
 	 * @return string       CSS for selected fonts
 	 */
 	public function get_css( $fonts ) {
-		$css = '';
+		$css = $this->sep;
 		$supplied_types =  wp_list_pluck( $fonts, 'type' );
+		$rule_types = $this->get_rule_types();
 		foreach ( $this->get_rules() as $type => $rules ) {
 			if ( empty( $rules ) || ! in_array( $type, $supplied_types) ) {
 				continue;
 			}
-			$font = wp_list_filter( $fonts, array( 'type' => $type ) );
-			$font = array_shift( $font );
-			$css .= $this->do_rules( $font, $rules );
+			$font = $this->list_entry_item_by( $fonts, array( 'type' => $type ) );
+			$rule_type = $this->list_entry_item_by( $this->get_rule_types(), array( 'id' => $type ) );
+			$css .= $this->do_rules( $font, $rules, $rule_type );
 		}
 		return $css;
 	}
 
 	/**
-	 * Return CSS for a specific type of font rules.
-	 * @param  array $font  Specified font for this set of rules
-	 * @param  array $rules Declared rules for this font rule
-	 * @return string       CSS for this font + rules
+	 * Like wp_list_filter, except the first matching item is returned.
+	 * @param  array $list     An array of objects to filter
+	 * @param array $args An array of key => value arguments to match against each object
+	 * @param string $operator The logical operation to perform:
+	 *    'AND' means all elements from the array must match;
+	 *    'OR' means only one element needs to match;
+	 *    'NOT' means no elements may match.
+	 *   The default is 'AND'.
+	 * @return mixed The first found object is successful, false if not.
 	 */
-	private function do_rules( $font, $rules ) {
+	private function list_entry_item_by( $list, $args, $operator = 'AND' ) {
+		$result = wp_list_filter( $list, $args );
+		if ( ! empty( $result ) ) {
+			return array_shift( $result );
+		}
+		return false;
+	}
+
+	/**
+	 * Return CSS for a specific type of font rules.
+	 * @param  array $font      Specified font for this set of rules
+	 * @param  array $rules     Declared rules for this font rule
+	 * @param  array $rule_type This set of $rules's type info
+	 * @return string           CSS for this font + rules
+	 */
+	private function do_rules( $font, $rules, $rule_type ) {
 		$css_rules = array();
 		foreach ( $rules as $rule ) {
-			$css  = $this->css_open( $rule );
-			$css .= $this->css_rules( $rule['rules'], $font );
+			$css = $this->css_open( $rule );
+			$css .= $this->css_rules( $rule['rules'], $font, $rule_type );
 			$css .= $this->css_close( $rule );
 			array_push( $css_rules, $css );
 		}
@@ -120,23 +165,104 @@ class Jetpack_Custom_Fonts_Css_Generator {
 	 * @param  array $font  A font for this font type
 	 * @return string       CSS (property: value)s
 	 */
-	private function css_rules( $rules, $font ) {
+	private function css_rules( $rules, $font, $type ) {
 		$css_rules = array();
-		$rule_sep = $this->sep ? ";\n" : ';';
+		$declaration_sep = ';' . $this->sep;
+		$rule_sep = $this->sep ? ': ' : ':';
+		$indent = $this->sep ? "\t" : '';
+		$rules = $this->shim_rules_for_type( $rules, $type );
+		mlog( $rules );
 		foreach( $rules as $rule ) {
 			switch( $rule['property'] ) {
-				case 'font-family' :
+				case 'font-family':
 					$value = $font['css_name'] . ',' . $rule['value'];
+					break;
+				case 'font-weight':
+					$value = $this->pick_weight( $font['fvds'] );
+					break;
+				case 'font-size':
+					$value = $this->maybe_scale_font( $rule['value'], $font );
+					break;
+				case 'font-style':
+					$value = $this->pick_style( $font['fvds'] );
 					break;
 				default:
 					$value = false;
 			}
 			if ( $value ) {
-				$css_rules[] = $rule['property'] . ':' . $value;
+				$css_rules[] = $indent . $rule['property'] . $rule_sep . $value;
 			}
 		}
-		return implode( $rule_sep, $css_rules );
+		return implode( $declaration_sep, $css_rules );
 	}
+
+	private function shim_rules_for_type( $rules, $type ) {
+		$needs_style = ! $this->list_entry_item_by( $rules, array( 'property' => 'font-style' ) );
+		$needs_weight = ! $this->list_entry_item_by( $rules, array( 'property' => 'font-weight' ) );
+		if ( $needs_style ) {
+			array_push( $rules, array( 'property' => 'font-style', 'value' => 'normal' ) );
+		}
+		if ( $needs_weight ) {
+			array_push( $rules, array( 'property' => 'font-weight', 'value' => 'normal' ) );
+		}
+		return $rules;
+	}
+
+	private function maybe_scale_font( $value, $font ) {
+		if ( ! isset( $font['size'] ) || ! $font['size'] ) {
+			return false;
+		}
+		$matches = array();
+		if ( ! preg_match( self::CSS_FONT_SIZE_VALUE_RX, $value, $matches ) ) {
+			return false;
+		}
+		$scale = (int) $font['size'] * 0.06 + 1;
+		$units = $matches[5];
+		if ( ! empty( $matches[4] ) ) {
+			// Integer size
+			$new_value = intval( $matches[4] );
+			$precision = ( $new_value > 9 ) ? 1 : 3;
+		} else {
+			// Floating point value
+			$new_value = floatval( $matches[2] );
+			$precision = strlen( $matches[3] ) + 1;
+		}
+		$new_value = round( $new_value * $scale, $precision );
+		return (string) $new_value . $units;
+	}
+
+	private function pick_weight( $fvds ) {
+		if ( 1 === count( $fvds ) ) {
+			return $this->get_weight_from_fvd( $fvds[0] );
+		}
+		$weights = array_map( array( $this, 'get_weight_from_fvd' ), $fvds );
+		asort( $weights );
+		return array_shift( $weights );
+	}
+
+
+	private function get_weight_from_fvd( $fvd ) {
+		return substr( $fvd, 1, 1 ) . '00';
+	}
+
+	private function pick_style( $fvds ) {
+		if ( 1 === count( $fvds ) ) {
+			return $this->get_style_from_fvd( $fvds[0] );
+		}
+		return 'normal';
+	}
+
+	private function get_style_from_fvd( $fvd ) {
+		switch ( substr( $fvd, 0, 1 ) ) {
+			case 'i':
+				return 'italic';
+			case 'o':
+				return 'oblique';
+			case 'n':
+				return 'normal';
+		}
+	}
+
 
 	/**
 	 * Open a CSS selector.
@@ -158,7 +284,7 @@ class Jetpack_Custom_Fonts_Css_Generator {
 	 * @return string      Closing of a CSS selector
 	 */
 	private function css_close( $rule ) {
-		$ret = '';
+		$ret = $this->sep;
 		if ( $rule['media_query'] ) {
 			$ret .= '}' . $this->sep;
 		}
