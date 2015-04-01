c9fb5a496f0ebc7e92fe588f21e955fd5034a9bf
Payton Swick <payton@foolord.com>
2015-04-01 18:34:12 -0400

    Allow additional provider Views to be added outside the plugin
    The default provider Views are an object in provider-views.js. Each value should
    be a Backbone View that will render its font option to the font list. Additional
    provider Views can be added by adding to the
    `wp.customize.JetpackFonts.providerViews` object.

--- js/helpers/provider-views.js
+++ js/helpers/provider-views.js
@@ -1,10 +1,24 @@
+var api = window.wp.customize;
+
 var DropdownItem = require( '../views/dropdown-item' );
 
+// The default provider Views. Each value should be a Backbone View that will
+// render its font option to the font list. Additional provider Views can be
+// added by adding to the `wp.customize.JetpackFonts.providerViews` object.
 var providerViews = {
 	google: DropdownItem.extend({})
 };
 
+function importProviderViews() {
+	if ( api.JetpackFonts && api.JetpackFonts.providerViews ) {
+		Object.keys( api.JetpackFonts.providerViews ).forEach( function( providerKey ) {
+			providerViews[ providerKey ] = api.JetpackFonts.providerViews[ providerKey ];
+		} );
+	}
+}
+
 function getViewForProvider( provider ) {
+	importProviderViews();
 	return providerViews[ provider ];
 }
 
