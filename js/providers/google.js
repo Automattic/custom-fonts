4753d3932f622dab953e93dc1ea87b8f2637602a
Payton Swick <payton@foolord.com>
2015-04-07 13:48:27 -0400

    Only load font characters needed by the menu

--- js/providers/google.js
+++ js/providers/google.js
@@ -3,19 +3,19 @@ var api = require( '../helpers/api' );
 
 var loadedFontIds = [];
 
-function addFontToPage( fontId ) {
-	if ( ~ loadedFontIds.indexOf( fontId ) ) {
+function addFontToPage( font ) {
+	if ( ~ loadedFontIds.indexOf( font.id ) ) {
 		return;
 	}
-	loadedFontIds.push( fontId );
-	WebFont.load( { google: { families: [ fontId ] } } );
+	loadedFontIds.push( font.id );
+	WebFont.load( { google: { families: [ font.id ], text: font.id } } );
 }
 
 var GoogleProviderView = api.JetpackFonts.ProviderView.extend({
 	render: function() {
 		this.$el.html( this.model.get( 'name' ) );
 		this.$el.css( 'font-family', '"' + this.model.get( 'name' ) + '"' );
-		addFontToPage( this.model.get( 'id' ) );
+		addFontToPage( this.model.toJSON() );
 		return this;
 	}
 });
