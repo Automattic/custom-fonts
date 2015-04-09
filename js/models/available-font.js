b9dcd54ffe5c754fe0bc5cce8e14b66ada4b39d1
Kate Ray <email@kateray.net>
2015-04-09 13:42:10 -0400

    Create model method for getting font weight options

--- js/models/available-font.js
+++ js/models/available-font.js
@@ -1,3 +1,9 @@
 var Backbone = require( '../helpers/backbone' );
 
-module.exports = Backbone.Model.extend({});
+module.exports = Backbone.Model.extend({
+	getFontWeightOptions: function(  ) {
+		if ( this.get( 'fvds' ) ) {
+			return this.get( 'fvds' );
+		}
+	}
+});
