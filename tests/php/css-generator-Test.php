324067ee47455f0657615994515433f47c839867
Payton Swick <payton@foolord.com>
2015-04-20 18:48:07 -0400

    Fix the get_css test

--- tests/php/css-generator-Test.php
+++ tests/php/css-generator-Test.php
@@ -27,7 +27,49 @@ function get_stylesheet() {
 	return 'twentyfourteen';
 }
 
-function do_action() {}
+function do_action( $action, $param ) {
+	if ( $action === 'jetpack_fonts_rules' ) {
+		$param->add_rule( array(
+			'type' => 'headings',
+			'selector' => '.entry-title',
+			'rules' => array(
+				array( 'property' => 'font-family', 'value' => 'inherit' ),
+				array( 'property' => 'font-size', 'value' => '33px' ),
+				array( 'property' => 'font-weight', 'value' => '300' ),
+			)
+		) );
+	}
+}
+
+function wp_list_filter( $list, $args = array(), $operator = 'AND' ) {
+	if ( ! is_array( $list ) )
+		return array();
+
+	if ( empty( $args ) )
+		return $list;
+
+	$operator = strtoupper( $operator );
+	$count = count( $args );
+	$filtered = array();
+
+	foreach ( $list as $key => $obj ) {
+		$to_match = (array) $obj;
+
+		$matched = 0;
+		foreach ( $args as $m_key => $m_value ) {
+			if ( array_key_exists( $m_key, $to_match ) && $m_value == $to_match[ $m_key ] )
+				$matched++;
+		}
+
+		if ( ( 'AND' == $operator && $matched == $count )
+		  || ( 'OR' == $operator && $matched > 0 )
+		  || ( 'NOT' == $operator && 0 == $matched ) ) {
+			$filtered[$key] = $obj;
+		}
+	}
+
+	return $filtered;
+}
 
 class Jetpack_Fonts_Css_Generator_Test extends PHPUnit_Framework_TestCase {
 	public function test_instance_exists() {
@@ -37,8 +79,23 @@ class Jetpack_Fonts_Css_Generator_Test extends PHPUnit_Framework_TestCase {
 
 	public function test_get_css_returns_text() {
 		$generator = new Jetpack_Fonts_Css_Generator;
-		$expected = '.site-title{ font-size: 32px; }';
-		$fonts_for_css = array();
-		$this->assertContains( $expected, $generator->get_css( $fonts_for_css ) );
+		$fonts_for_css = array(
+			array(
+				'type' => 'headings',
+				'name' => 'Lobster Two',
+				'id' => 'Lobster+Two',
+				'fvds' => array(
+					'n4' => 'Regular',
+					'i4' => 'Italic'
+				),
+				'subsets' => array(
+					'latin'
+				),
+				'bodyText' => false,
+				'css_name' => 'Lobster Two'
+			)
+		);
+		// mock $generator->get_rules, which we do above with do_action
+		$this->assertRegExp( '/\.entry-title\{/', $generator->get_css( $fonts_for_css ) );
 	}
 }
