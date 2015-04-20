36c187061c1cdc0dd2e4f2960c0942451f609a2e
Payton Swick <payton@foolord.com>
2015-04-20 17:43:18 -0400

    Add basic passing test for Jetpack_Fonts_Css_Generator

--- tests/php/css-generator-Test.php
+++ tests/php/css-generator-Test.php
@@ -2,9 +2,20 @@
 
 include dirname( __FILE__ ) . '/../../css-generator.php';
 
+function __( $args ) {
+	return $args;
+}
+
+function apply_filters( $arg1, $arg2 ) {
+	$arg1;
+	return $arg2;
+}
+
+function add_action() {}
+
 class Jetpack_Fonts_Css_Generator_Test extends PHPUnit_Framework_TestCase {
 	public function test_instance_exists() {
 		$generator = new Jetpack_Fonts_Css_Generator;
-		$this->assertTrue( $generator );
+		$this->assertTrue( (boolean)$generator );
 	}
 }
