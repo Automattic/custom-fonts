43936295c587eeb9e19ca331c2c99c0d6a8661ca
Payton Swick <payton@foolord.com>
2015-04-21 17:57:24 -0400

    Add Oblique to getFontVariantNameFromId

--- js/helpers/fvd-to-readable.js
+++ js/helpers/fvd-to-readable.js
@@ -1,26 +1,39 @@
 var styleOptions = {
 	'n1': 'Thin',
 	'i1': 'Thin Italic',
+	'o1': 'Thin Oblique',
 	'n2': 'Extra Light',
 	'i2': 'Extra Light Italic',
+	'o2': 'Extra Light Oblique',
 	'n3': 'Light',
 	'i3': 'Light Italic',
+	'o3': 'Light Oblique',
 	'n4': 'Regular',
 	'i4': 'Italic',
+	'o4': 'Oblique',
 	'n5': 'Medium',
 	'i5': 'Medium Italic',
+	'o5': 'Medium Oblique',
 	'n6': 'Semibold',
 	'i6': 'Semibold Italic',
+	'o6': 'Semibold Oblique',
 	'n7': 'Bold',
 	'i7': 'Bold Italic',
+	'o7': 'Bold Oblique',
 	'n8': 'Extra Bold',
 	'i8': 'Extra Bold Italic',
+	'o8': 'Extra Bold Oblique',
 	'n9': 'Black',
-	'i9': 'Black Italic'
+	'i9': 'Black Italic',
+	'o9': 'Black Oblique'
 };
 
 module.exports = {
 	getFontVariantNameFromId: function( id ) {
-		return styleOptions[ id ];
+		var match = styleOptions[ id ];
+		if ( match ) {
+			return match;
+		}
+		return 'Regular';
 	}
 };
--- tests/js/fvd-to-readable.js
+++ tests/js/fvd-to-readable.js
@@ -22,12 +22,20 @@ describe( 'FvdToReadable', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i1' ) ).to.equal( 'Thin Italic' );
 		} );
 
+		it( 'returns Thin Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o1' ) ).to.equal( 'Thin Oblique' );
+		} );
+
 		it( 'returns Extra Light correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'n2' ) ).to.equal( 'Extra Light' );
 		} );
 
 		it( 'returns Extra Light Italic correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i2' ) ).to.equal( 'Extra Light Italic' );
+		} );
+
+		it( 'returns Extra Light Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o2' ) ).to.equal( 'Extra Light Oblique' );
 		} );
 
 		it( 'returns Light correctly', function() {
@@ -38,12 +46,20 @@ describe( 'FvdToReadable', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i3' ) ).to.equal( 'Light Italic' );
 		} );
 
+		it( 'returns Light Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o3' ) ).to.equal( 'Light Oblique' );
+		} );
+
 		it( 'returns Regular correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'n4' ) ).to.equal( 'Regular' );
 		} );
 
 		it( 'returns Italic correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i4' ) ).to.equal( 'Italic' );
+		} );
+
+		it( 'returns Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o4' ) ).to.equal( 'Oblique' );
 		} );
 
 		it( 'returns Medium correctly', function() {
@@ -54,12 +70,20 @@ describe( 'FvdToReadable', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i5' ) ).to.equal( 'Medium Italic' );
 		} );
 
+		it( 'returns Medium Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o5' ) ).to.equal( 'Medium Oblique' );
+		} );
+
 		it( 'returns Semibold correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'n6' ) ).to.equal( 'Semibold' );
 		} );
 
 		it( 'returns Semibold Italic correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i6' ) ).to.equal( 'Semibold Italic' );
+		} );
+
+		it( 'returns Semibold Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o6' ) ).to.equal( 'Semibold Oblique' );
 		} );
 
 		it( 'returns Bold correctly', function() {
@@ -70,6 +94,10 @@ describe( 'FvdToReadable', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i7' ) ).to.equal( 'Bold Italic' );
 		} );
 
+		it( 'returns Bold Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o7' ) ).to.equal( 'Bold Oblique' );
+		} );
+
 		it( 'returns Extra Bold correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'n8' ) ).to.equal( 'Extra Bold' );
 		} );
@@ -78,12 +106,24 @@ describe( 'FvdToReadable', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i8' ) ).to.equal( 'Extra Bold Italic' );
 		} );
 
+		it( 'returns Extra Bold Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o8' ) ).to.equal( 'Extra Bold Oblique' );
+		} );
+
 		it( 'returns Black correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'n9' ) ).to.equal( 'Black' );
 		} );
 
 		it( 'returns Black Italic correctly', function() {
 			expect( FvdToReadable.getFontVariantNameFromId( 'i9' ) ).to.equal( 'Black Italic' );
+		} );
+
+		it( 'returns Black Oblique correctly', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'o9' ) ).to.equal( 'Black Oblique' );
+		} );
+
+		it( 'returns Regular if no match is found', function() {
+			expect( FvdToReadable.getFontVariantNameFromId( 'x9' ) ).to.equal( 'Regular' );
 		} );
 
 	} );
