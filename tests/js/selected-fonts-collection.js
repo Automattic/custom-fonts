02f0e29250e639cf8131ed00b4da2c2530827d99
Payton Swick <payton@foolord.com>
2015-06-02 10:57:04 -0400

    Add test for two of the same font

--- tests/js/selected-fonts-collection.js
+++ tests/js/selected-fonts-collection.js
@@ -11,10 +11,12 @@ describe( 'SelectedFonts', function() {
 		helpers.before();
 		mockery.registerMock( '../models/selected-font', Backbone.Model );
 		var SelectedFonts = require( '../../js/collections/selected-fonts' );
-		selectedFonts = new SelectedFonts();
-		selectedFonts.add( { type: 'one', id: 'foobar', displayName: 'foobar' } );
-		selectedFonts.add( { type: 'one', id: 'barfoo', displayName: 'barfoo' } );
-		selectedFonts.add( { type: 'test', displayName: 'test' } );
+		selectedFonts = new SelectedFonts( [
+			{ type: 'one', id: 'foobar', displayName: 'foobar' },
+			{ type: 'two', id: 'foobar', displayName: 'foobar' },
+			{ type: 'one', id: 'barfoo', displayName: 'barfoo' },
+			{ type: 'test', displayName: 'test' }
+		] );
 	} );
 
 	after( helpers.after );
@@ -30,6 +32,11 @@ describe( 'SelectedFonts', function() {
 
 		it( 'does not return models without an id', function() {
 			expect( selectedFonts.toJSON() ).to.not.include( { type: 'test', displayName: 'test' } );
+		} );
+
+		it( 'returns all models in the collection which have the same id', function() {
+			expect( selectedFonts.toJSON() ).to.include( { id: 'foobar', type: 'one', displayName: 'foobar' } );
+			expect( selectedFonts.toJSON() ).to.include( { id: 'foobar', type: 'two', displayName: 'foobar' } );
 		} );
 	} );
 } );
