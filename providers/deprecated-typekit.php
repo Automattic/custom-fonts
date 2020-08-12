d62fb7dd47eadf12b4e9270f086d827cd160065c
Glen Davies <glen.davies@a8c.com>
2020-08-12 15:44:24 +1200

    Start adding all the font mappings

--- providers/deprecated-typekit.php
+++ providers/deprecated-typekit.php
@@ -7,34 +7,70 @@
 class Jetpack_Fonts_Typekit_Font_Mapper {
 
 	private static $mappings = array(
-		'gmsj' => array(
-			'id'            => 'Roboto+Slab',
-			'cssName'       => 'Roboto Slab',
-			'provider'      => 'google',
-			'genericFamily' => 'serif',
-			'fvds'          => array( 'n1', 'n3', 'n4', 'n7' ),
-		),
 		'gjst' => array(
 			'id'            => 'Lora',
 			'cssName'       => 'Lora',
-			'provider'      => 'google',
 			'genericFamily' => 'serif',
 			'fvds'          => array( 'n4', 'i4', 'n7', 'i7' ),
+		),
+		'gmsj' => array(
+			'id'            => 'Roboto+Slab',
+			'cssName'       => 'Roboto Slab',
+			'genericFamily' => 'serif',
+			'fvds'          => array( 'n1', 'n3', 'n4', 'n7' ),
+		),
+		'sskw' => array(
+			'id'            => 'Old+Standard+TT',
+			'cssName'       => 'Old Standard TT',
+			'genericFamily' => 'serif',
+			'fvds'          => array( 'n4', 'i4', 'n7' ),
+		),
+		'tsyb' => array(
+			'id'            => 'Arvo',
+			'cssName'       => 'Arvo',
+			'genericFamily' => 'serif',
+			'fvds'          => array( 'n4', 'i4', 'n7', 'i7' ),
+		),
+		'yvxn' => array(
+			'id'            => 'Lato',
+			'cssName'       => 'Lato',
+			'genericFamily' => 'sans-serif',
+			'fvds'          => array( 'n1', 'i1', 'n3', 'i3', 'n4', 'i4', 'n7', 'i7', 'n9', 'i9' ),
+		),
+		'ymzk' => array(
+			'id'            => 'Amaranth',
+			'cssName'       => 'Amaranth',
+			'genericFamily' => 'sans-serif',
+			'fvds'          => array( 'n4', 'i4', 'n7', 'i7' ),
+		),
+		'vybr' => array(
+			'id'            => 'Crimson+Text',
+			'cssName'       => 'Crimson Text',
+			'genericFamily' => 'sans-serif',
+			'fvds'          => array( 'n4', 'i4', 'n6', 'i6', 'n7', 'i7' ),
 		),
 	);
 
 	public static function get_mapped_google_font( $font ) {
 		if ( array_key_exists( $font['id'], self::$mappings ) ) {
 			$mapped_font = self::$mappings[ $font['id'] ];
+			$new_font    = array(
+				'id'            => $mapped_font['id'],
+				'provider'      => 'google',
+				'cssName'       => $mapped_font['cssName'],
+				'genericFamily' => $mapped_font['genericFamily'],
+				'type'          => $font['type'],
+			);
+
 			if ( isset( $font['currentFvd'] ) ) {
-				$mapped_font['currentFvd'] = self::valid_or_closest_fvd_for_font( $font['currentFvd'], $mapped_font['fvds'] );
+				$new_font['currentFvd'] = self::valid_or_closest_fvd_for_font( $font['currentFvd'], $mapped_font['fvds'] );
 			}
-			unset( $mapped_font['fvds'] );
+
 			if ( isset( $font['size'] ) ) {
-				$mapped_font['size'] = $font['size'];
+				$new_font['size'] = $font['size'];
 			}
-			$mapped_font['type'] = $font['type'];
-			return $mapped_font;
+
+			return $new_font;
 		}
 	}
 
