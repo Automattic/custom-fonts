64d7b60175f80b56fba03141b63e6c92f17a646e
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2023-02-20 16:17:21 +0200

    Guard against block themes and move isset check at the beginning of the function.

--- gutenberg-integration.php
+++ gutenberg-integration.php
@@ -7,15 +7,15 @@
  * @return array
  */
 function custom_fonts_append_font_configuration_in_gutenberg_editor( $settings ) {
+	if ( ! isset( $settings['styles'][0]['css'] ) || function_exists( 'wp_is_block_theme' ) && wp_is_block_theme() ) {
+		return $settings;
+	}
+
 	require_once __DIR__ . '/css-generator.php';
 
 	$generator = new Jetpack_Fonts_Css_Generator( '' );
 	$fonts = new Jetpack_Fonts( $generator );
 	$fonts->register_providers();
-
-	if ( ! isset( $settings['styles'][0]['css'] ) ) {
-		return $settings;
-	}
 
 	$settings['styles'][0]['css'] .= $fonts->get_font_css();
 
@@ -30,6 +30,10 @@ add_filter( 'block_editor_settings_all' , 'custom_fonts_append_font_configuratio
  * @return void
  */
 function enqueue_font_loader_script_in_gutenberg() {
+	if ( function_exists( 'wp_is_block_theme' ) && wp_is_block_theme() ) {
+		return;
+	}
+
 	$fonts = Jetpack_Fonts::get_instance();
 	$fonts->register_providers();
 
